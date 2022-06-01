7b5954b2983adf2f9279eda66f321ea9bc44293e
discrate <88935250+discrate@users.noreply.github.com>
2022-06-01 09:45:41 +0200

    append vizsgaremek files

---
+++ test_conduit/input_test_data.py
@@ -0,0 +1,26 @@
+user1 = {
+    "name": "szgteszt1",
+    "email": "szgteszt1@gmail.com",
+    "password": "Tesztelek1",
+    "article1": "Nekünk kell egymással és önmagunk lelkéből tüzet rakni. Hogy melegedhessünk körülötte. Ott, ahol akármilyen kis körben, "
+                "de szeretet van, jó lesz élni. Ott meghittség lesz. Barátságos világ. Boldogító légkör.",
+    "comment1": "ez egy világos hozzászólás",
+}
+
+user2 = {
+    "name": "szgteszt2",
+    "email": "szgteszt2@gmail.com",
+    "password": "Tesztelek2",
+    "article2":"Az összevisszaságban találd meg az egyszerűséget, a hangzavarban a harmóniát. A nehézségek közt mindig ott van a lehetőség.",
+    "comment2": "ez egy értelmes hozzászólás",
+}
+
+user = {
+    "counter": 990,
+    "name": "szgtest",
+    "domain": "gmail.com",
+    "password": "Tesztelek1"
+}
+
+# Alap profilkép:
+# https://static.productionready.io/images/smiley-cyrus.jpg
