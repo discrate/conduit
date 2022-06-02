bae21b6c8f2a2e1522725bf0b8ab54c6c50ad38d
discrate <88935250+discrate@users.noreply.github.com>
2022-06-02 23:09:55 +0200

    update input

--- test_conduit/input_test_data.py
+++ test_conduit/input_test_data.py
@@ -2,16 +2,13 @@ user1 = {
     "name": "szgteszt1",
     "email": "szgteszt1@gmail.com",
     "password": "Tesztelek1",
-    "article1": "Nekünk kell egymással és önmagunk lelkéből tüzet rakni. Hogy melegedhessünk körülötte. Ott, ahol akármilyen kis körben, "
-                "de szeretet van, jó lesz élni. Ott meghittség lesz. Barátságos világ. Boldogító légkör.",
-    "comment1": "ez egy világos hozzászólás",
 }
 
 user2 = {
     "name": "szgteszt2",
     "email": "szgteszt2@gmail.com",
     "password": "Tesztelek2",
-    "article2":"Az összevisszaságban találd meg az egyszerűséget, a hangzavarban a harmóniát. A nehézségek közt mindig ott van a lehetőség.",
+    "article2": "Az összevisszaságban találd meg az egyszerűséget, a hangzavarban a harmóniát. A nehézségek közt mindig ott van a lehetőség.",
     "comment2": "ez egy értelmes hozzászólás",
 }
 
@@ -22,5 +19,22 @@ user = {
     "password": "Tesztelek1"
 }
 
+article = {
+    "title": "cím2",
+    "about": "valami2",
+    "body": "idézet szövege2",
+    "tag": "valami2"
+}
+
+#     "title": "Müller Péter",
+#     "about": "idézet",
+#     "body": "Nekünk kell egymással és önmagunk lelkéből tüzet rakni. Hogy melegedhessünk körülötte. Ott, ahol akármilyen kis körben, "
+#             "de szeretet van, jó lesz élni. Ott meghittség lesz. Barátságos világ. Boldogító légkör.",
+#     "tag": "útravaló"
+
+
+comment = {
+    "comment1": "ez egy világos hozzászólás"
+}
 # Alap profilkép:
 # https://static.productionready.io/images/smiley-cyrus.jpg
