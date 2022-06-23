1f38888550312fa09707b1e85f28ead32ce0952d
discrate <88935250+discrate@users.noreply.github.com>
2022-06-23 19:41:29 +0200

    assert update

--- test_conduit/test_main.py
+++ test_conduit/test_main.py
@@ -34,12 +34,8 @@ class TestConduit(object):
         time.sleep(2)
         result_message = self.browser.find_element_by_xpath('//div[@class="swal-title"]')
         result_reason = self.browser.find_element_by_xpath('//div[@class="swal-text"]')
-        try:
-            assert result_message.text == messages["failed"]
-            assert result_reason.text == messages["valid"]
-            print("Helyes a kapott hibaüzenet")
-        except AssertionError:
-            print("Hiányzó elutasító üzenet")
+        assert result_message.text == messages["failed"]
+        assert result_reason.text == messages["valid"]
 
     # ------------------------------------------------------------------------------------------------------------------
     # // Teszteset 02 \\ Regisztráció helyes adatokkal (létrehozott random felhasználónévvel és random email címmel)
@@ -60,12 +56,8 @@ class TestConduit(object):
         time.sleep(2)
         result_message = self.browser.find_element_by_xpath('//div[@class="swal-title"]')
         result_reason = self.browser.find_element_by_xpath('//div[@class="swal-text"]')
-        try:
-            assert result_message.text == messages["welcome"]
-            assert result_reason.text == messages["success"]
-            print('Sikeres regisztráció')
-        except AssertionError:
-            print('Sikertelen regisztráció')
+        assert result_message.text == messages["welcome"]
+        assert result_reason.text == messages["success"]
         ok_btn = self.browser.find_element_by_xpath('//button[@class="swal-button swal-button--confirm"]')
         ok_btn.click()
 
@@ -82,25 +74,17 @@ class TestConduit(object):
         sign_in_btn.click()
         time.sleep(2)
         user_profile = self.browser.find_elements_by_xpath('//a[@class="nav-link"]')[2]
-        try:
-            assert user_profile.text == self.random_name
-            print('Sikeres bejelentkezés')
-        except AssertionError:
-            print('Nem sikerült bejelentkezni')
+        assert user_profile.text == self.random_name
 
-        # ------------------------------------------------------------------------------------------------------------------
-        # // Teszteset 04 \\ Adatkezelési nyilatkozat használata (cookiek elfogadása)
+    # ------------------------------------------------------------------------------------------------------------------
+    # // Teszteset 04 \\ Adatkezelési nyilatkozat használata (cookiek elfogadása)
 
     def test_accept_cookies(self):
         accept_btn = self.browser.find_element_by_xpath('//div[normalize-space()="I accept!"]')
         accept_btn.click()
         time.sleep(1)
         decline_btn_list = self.browser.find_elements_by_xpath('//div[normalize-space()="I decline!"]')
-        try:
-            assert len(decline_btn_list) == 0
-            print("Cookie elfogadás rendben")
-        except AssertionError:
-            print('Hiba merült fel a cookie-kal kapcsolatban.')
+        assert len(decline_btn_list) == 0
 
     # ------------------------------------------------------------------------------------------------------------------
     # # // Teszteset 05 \\ Adatok listázása ("Popular tag"-ek listázása)
@@ -109,11 +93,7 @@ class TestConduit(object):
         list_of_tags = []
         for i, j in enumerate(popular_tags):
             list_of_tags.append(f'{i + 1}. elem: {j.text}')
-        try:
-            assert len(list_of_tags) == len(popular_tags)
-            print(f'Helyes lista, elemek száma: {len(list_of_tags)}')
-        except AssertionError:
-            print('Helytelen lista')
+        assert len(list_of_tags) == len(popular_tags)
 
     # ------------------------------------------------------------------------------------------------------------------
     # # // Teszteset 06 \\ Több oldalas lista bejárása (főoldal alján levő navigációs sáv bejárása)
@@ -124,11 +104,7 @@ class TestConduit(object):
             page_button = index_page_list[i]
             page_button.click()
         time.sleep(1)
-        try:
-            assert index_page_list[-1].text == f'{len(index_page_list)}'
-            print('Számozás rendben')
-        except AssertionError:
-            print('Számozás nincs rendben')
+        assert index_page_list[-1].text == f'{len(index_page_list)}'
 
     # ------------------------------------------------------------------------------------------------------------------
     # # // Teszteset 07 \\ Új adatbevitel (új cikk létrehozása)
@@ -149,11 +125,7 @@ class TestConduit(object):
         publish_article_btn.click()
         time.sleep(2)
         created_body = self.browser.find_element_by_xpath('//p')
-        try:
-            assert created_body.text == article['body']
-            print('Helyesen létrehozva')
-        except AssertionError:
-            print('Helytelen cikk')
+        assert created_body.text == article['body']
 
     # ------------------------------------------------------------------------------------------------------------------
     # // Teszteset 08 \\ Ismételt és sorozatos adatbevitel adatforrásból
@@ -191,11 +163,7 @@ class TestConduit(object):
         img_source = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH,
                                                                                           '//img[@class="user-img"]'))).get_attribute(
             "src")
-        try:
-            assert img_source == profile_pic['Rick1']
-            print("Sikeres képcsere")
-        except AssertionError:
-            print("Sikertelen képcsere")
+        assert img_source == profile_pic['Rick1']
 
     # ------------------------------------------------------------------------------------------------------------------
     # # // Teszteset 10 \\ Adat vagy adatok törlése (komment hozzáadása, majd eltávolítása)
@@ -213,11 +181,7 @@ class TestConduit(object):
         delete_btn.click()
         time.sleep(1)
         comments_list_after = self.browser.find_elements_by_xpath('//div[@class="card"]')
-        try:
-            assert len(comments_list_after) == len(comments_list_before)
-            print("Comment eltávolítva")
-        except AssertionError:
-            print("Hiba történt a comment törlésekor.")
+        assert len(comments_list_after) == len(comments_list_before)
 
     # ------------------------------------------------------------------------------------------------------------------
     # // Teszteset 11 \\ Adatok lementése felületről (Popular tag-ek kimentése .txt file-ba)
@@ -230,11 +194,7 @@ class TestConduit(object):
             for i, j in enumerate(popular_tags):
                 output_f.write(f'{i + 1}. tag: {j.text}' "\n")
                 count += 1
-        try:
-            assert count == len(popular_tags)
-            print("Popular tag lista rendben")
-        except AssertionError:
-            print("Popular tag lista helytelen")
+        assert count == len(popular_tags)
 
     # ------------------------------------------------------------------------------------------------------------------
     # # // Teszteset 12 \\ Kijelentkezés (felhasználó kijelentkezése)
@@ -243,8 +203,4 @@ class TestConduit(object):
         logout_btn = self.browser.find_element_by_xpath('//a[@active-class="active"]')
         logout_btn.click()
         home_sign_in_btn = self.browser.find_elements_by_xpath('//a[@href="#/login"]')[0]
-        try:
-            assert home_sign_in_btn.text == "Sign in"
-            print('Sikeres kijelentkezés')
-        except AssertionError:
-            print('Nem sikerült kijelentkezni')
+        assert home_sign_in_btn.text == "Sign in"
